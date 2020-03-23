8ca0d144bc47de816f3265f92cb03520d6ed1daf
Gonzalez <c.d.gonzalez@accenture.com>
2020-03-23 11:23:21 -0600

    fixed main js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,38 @@
-(function() {
-    "use strict";
+"use strict";
 
-    var regalo = document.getElementById('regalo');
-    document.addEventListener('DOMContentLoaded', function(){
+var regalo = document.getElementById("regalo");
+document.addEventListener("DOMContentLoaded", function() {
+  //Datos Usuario
+  var nombre = document.getElementById("nombre");
+  var apellido = document.getElementById("apellido");
+  var email = document.getElementById("email");
 
-        //Datos Usuario
-        var nombre = document.getElementById('nombre');
-        var apellido = document.getElementById('apellido');
-        var email = document.getElementById('email');
+  //Campos Pases
+  var pase_dia = document.getElementById("pase_dia");
+  var pase_completo = document.getElementById("pase_completo");
+  var pase_dosdias = document.getElementById("pase_dosdias");
 
+  //Botones y divs
+  var calcular = document.getElementById("calcular");
+  var errorDiv = document.getElementById("error");
+  var botonRegistro = document.getElementById("btnRegistro");
+  var resultado = document.getElementById("lista-productos");
 
-        //Campos Pases
-        var pase_dia = document.getElementById('pase_dia');
-        var pase_completo = document.getElementById('pase_completo');
-        var pase_dosdias = document.getElementById('pase_dosdias');
+  calcular.addEventListener("click", calcularMontos);
 
-        
-        //Botones y divs
-        var calcular = document.getElementById('calcular');
-        var errorDiv = document.getElementById('error');
-        var botonRegistro = document.getElementById('btnRegistro');
-        var resultado = document.getElementById('lista-productos');
+  function calcularMontos(event) {
+    event.preventDefault();
+    if (regalo.value === "") {
+      alert("Debes Elegir un Regalo.");
+      regalo.focus();
+    } else {
+      var boletosDia = pase_dia.value;
+      var boletos2Dias = pase_dosdias.value;
+      var boletoComlpeto = pase_completo.value;
 
-        calcular.addEventListener('click', calcularMontos);
-
-        function calcularMontos(event) {
-            event.preventDefault();
-            if(regalo.value === '') {
-                alert('Debes Elegir un Regalo.');
-                regalo.focus();
-            } else {
-                var boletosDia = pase_dia.value;
-                var boletos2Dias = pase_dosdias.value;
-                var boletoComlpeto = pase_completo.value;
-
-                var totalPagar = (boletosDia * 30) + ( boletos2Dias * 45) + (boletoComlpeto * 50);
-                console.log(totalPagar);
-
-            }
-            
-        }
-
-
-
-    });
-})
+      var totalPagar =
+        boletosDia * 30 + boletos2Dias * 45 + boletoComlpeto * 50;
+      console.log(totalPagar);
+    }
+  }
+});
